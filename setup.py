1039069fda355c166e2678bda0169c9ecc6d8498
Arewa Olakunle <danceasarxx@gmail.com>
2015-07-10 13:43:45 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='micron',
+    version='1.0',
+    packages=['micron'],
+    url='',
+    license='MIT',
+    author='Arewa Olakunle',
+    author_email='arewa.olakunle@gmail.com',
+    description='A simple framework built out of webapp2 that helps to support flask-like routes'
+)
